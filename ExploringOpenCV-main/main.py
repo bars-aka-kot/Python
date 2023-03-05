9d14803bd04b2bcddcf33961ceaee07aa401a435
bars_aka_kot <basirov_ilgiz@mail.ru>
2023-03-05 22:51:12 +0500

    add ExploringOpenCV

--- ExploringOpenCV-main/main.py
+++ ExploringOpenCV-main/main.py
@@ -1,8 +1,9 @@
 import cv2
 
-img = cv2.imread('D:/GB/bootcamp/ExploringOpenCV/test.jpg')
+img = cv2.imread(
+    'D:/GeekBrains/HomeLessons/Python/ExploringOpenCV-main/test2.jpeg')
 
-#img = cv2.resize(img, (500, 500))
+# img = cv2.resize(img, (500, 500))
 
 cv2.imshow('Result', img)
 
