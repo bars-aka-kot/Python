2fb539b7472f9302063676d53fcd816501f0a8e8
bars_aka_kot <basirov_ilgiz@mail.ru>
2023-04-13 20:39:59 +0500

    modified main.py and Sem1/task2

--- FirstExperience/main.py
+++ FirstExperience/main.py
@@ -48,6 +48,17 @@
 #         line += "*"
 #     print(line)
 
+'''Подстановка окончаний в зависимости от числа
+words = ['день', 'дня', 'дней']
+
+if all((value % 10 == 1, value % 100 != 11)):
+    return words[0]
+elif all((2 <= value % 10 <= 4,
+          any((value % 100 < 10, value % 100 >= 20)))):
+    return words[1]
+return words[2]
+'''
+
 # срезы
 """
 text = "СъЕШЬ ещё этих МяГкИх французских булок"
--- Sem1/task2.py
+++ Sem1/task2.py
@@ -10,3 +10,11 @@ while n > 0:
     sum = sum + x
     n = n // 10
 print(f"The sum of the numbers of the number {n} -> {sum}")
+
+# a = int(input())
+# n3 = a%10
+# n2 = a//10%10
+# n1 = a//100
+# print(n1+n2+n3)
+# a = input()
+# print(int(a[0])+int(a[1])+int(a[2]))
